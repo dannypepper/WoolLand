3022208e7dc293bc4249f790903329b45c9d4b82
dannypepper <bors.dani@gmail.com>
2022-06-21 10:21:15 +0200

    Add brand logo to Home page

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <div class="name">
+      <div class="letter">
+        <img src="../assets/logos/logo.svg" alt="Brand logo">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
